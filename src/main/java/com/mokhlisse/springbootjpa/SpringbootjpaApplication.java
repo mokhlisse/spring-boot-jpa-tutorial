f952394f5e701ff2a4eb4429d4d11c044b7e1e4c
badre edine <mokhlisse.badre@gmail.com>
2018-07-20 17:14:19 +0100

    chanage to master branch

--- src/main/java/com/mokhlisse/springbootjpa/SpringbootjpaApplication.java
+++ src/main/java/com/mokhlisse/springbootjpa/SpringbootjpaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootjpaApplication {
 
+	// change to master
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootjpaApplication.class, args);
 	}
